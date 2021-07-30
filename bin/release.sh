22f7441c29fff6ca0ac1b8ef091b5705fe88ce9e
edumerckx <test@test>
2021-07-30 19:18:46 -0300

    fix sh

--- bin/release.sh
+++ bin/release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ $1 != "major" ] || [ $1 != "minor" ] || [ $1 != "patch" ]
+if [ $1 != "major" ] && [ $1 != "minor" ] && [ $1 != "patch" ]
 then
   echo "Allowed values are: major, minor or patch" && 
   exit 1
@@ -9,7 +9,7 @@ fi
 git config user.name edumerckx
 git config user.email test@test
 
-yarn version $1 &&
+yarn version --$1 &&
   git push https://edumerckx:${PASS}@github.com/edumerckx/demo-gh-actions.git HEAD:main --tags &&
   echo "Successfully released new version!" &&
   exit 0
